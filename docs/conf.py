a3f7066cbc00ae660abb3be9c0e6edfba22f1525
Endermanik13 <119730649+Endermanik13@users.noreply.github.com>
2025-05-16 11:13:25 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,9 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = []
-
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
 language = 'ru'
+
+def setup(app):
+    app.add_css_file('custom.css')
