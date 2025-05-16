7baa4086fae1942957ea1cea0785c1baf2d784f5
Endermanik13 <119730649+Endermanik13@users.noreply.github.com>
2025-05-16 11:08:01 +0300

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,22 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'MyProject'
+copyright = '2025, Your Name'
+author = 'Your Name'
+release = '1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+language = 'ru'
